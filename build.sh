82ad9b864c1a59240c591f86bf51c19b94115fd0
EuCaue <souzacaue@proton.me>
2025-09-28 22:12:23 -0300

    build: improve `build.sh` and force `PROD=true` at build time

--- build.sh
+++ build.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
+EXT_NAME="reddit-auto-theme"
+OUTPUT_DIR="dist"
+SRC_DIR="$PWD/src"
+TMP_DIR="$(mktemp -d)"
+
+cp -r "$SRC_DIR"/* "$TMP_DIR"
+
+# force PROD = true on all files
+find "$TMP_DIR" -type f -name "*.js" -exec sed -i 's/^const PROD = .*/const PROD = true;/' {} +
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 RESET='\033[0m'
 
-echo -e "${BLUE}Building...${RESET}"
+echo -e "${BLUE}Building extension...${RESET}"
 
-web-ext build -s "./src" -a "dist" --overwrite-dest -n "reddit-auto-theme.zip"
+if web-ext build -s "$TMP_DIR" -a "$OUTPUT_DIR" --overwrite-dest -n "$EXT_NAME.zip"; then
+  echo -e "${GREEN}Build complete.${RESET}"
+  echo -e "Output: $PWD/$OUTPUT_DIR/$EXT_NAME.zip"
+else
+  echo -e "${RED}Build failed.${RESET}"
+  exit 1
+fi
 
-echo -e "${GREEN}All done.${RESET}"
+# clear tmp files
+rm -rf "$TMP_DIR"
